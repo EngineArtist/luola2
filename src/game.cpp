3e800552eb2cdadfeacbbb84a17e6a0f3033f6b9
Calle Laakkonen <calle@luolamies.org>
2012-11-12 21:53:46 +0200

    Terrain test code

--- src/game.cpp
+++ src/game.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <cmath>
 
 #include <GL/glew.h>
 #include <GL/glfw.h>
@@ -13,12 +14,50 @@
 
 #include "physics.h"
 #include "game.h"
+#include "terrain/terrain.h"
 
 void bounds(const glm::vec2 pos, glm::vec2 &vel) {
     if(pos.x<-10 || pos.x>10)
         vel.x = -vel.x;
     if(pos.y<-7.5 || pos.y>7.5)
         vel.y = -vel.y;
+}
+
+std::vector<terrain::ConvexPolygon> makeTerrain()
+{
+    using namespace terrain;
+    std::vector<ConvexPolygon> polys;
+    std::vector<Point> points;
+#if 1
+    for(int i=0;i<6;++i) {
+        points.push_back(Point(cos(i/6.0 * M_PI * 2) * 3, sin(i/6.0 * M_PI * 2) * 3));
+        std::cerr << points[points.size()-1].x << ", " << points[points.size()-1].y << std::endl;
+    }
+#else
+    points.push_back(Point(-2, 1.2));
+    points.push_back(Point(-2, -1.2));
+    points.push_back(Point(3, -0));
+#endif
+    polys.push_back(points);
+
+    return polys;
+}
+
+terrain::ConvexPolygon makeNibblePolygon(float x, float y)
+{
+    using namespace terrain;
+    std::vector<Point> points;
+#if 1
+    for(int i=0;i<6;++i) {
+        points.push_back(Point(cos(i/6.0 * M_PI * 2) * 0.5 + x, sin(i/6.0 * M_PI * 2) * 0.5 + y));
+    }
+#else
+    points.push_back(Point(x-0.5, y+1));
+    points.push_back(Point(x-0.5, y-1));
+    points.push_back(Point(x+0.5, y-1));
+    points.push_back(Point(x+0.5, y+1));
+#endif
+    return ConvexPolygon(points);
 }
 
 void gameloop()
@@ -29,7 +68,7 @@ void gameloop()
     {
         DataFile df("core.data");
         if(df.isError()) {
-            std::cerr << df.errorString() << "\n";
+            std::cerr << "core data error: " << df.errorString() << "\n";
             return;
         }
 
@@ -74,6 +113,10 @@ void gameloop()
     double time_now = glfwGetTime();
     double time_accumulator = 0.0;
 
+    terrain::Terrain terrain(makeTerrain());
+    terrain.updateGl();
+
+    bool mousedown = false;
     do {
         double new_time = glfwGetTime();
         double frame_time = new_time - time_now;
@@ -117,8 +160,29 @@ void gameloop()
                 objects[i].addImpulse(glm::vec2(0.2, 0));
         }
 
+        if(glfwGetMouseButton(GLFW_MOUSE_BUTTON_LEFT)) {
+            if(!mousedown) {
+                mousedown = true;
+                int mx, my;
+                glfwGetMousePos(&mx, &my);
+                float x, y;
+                x = (mx - 400) / 400.0 * 10;
+                y = -(my - 300) / 300.0 * 7.5;
+                std::cout << "mouse " << mx << ", " << my << " --> " << x << ", " << y << std::endl;
+                bool dirty = terrain.nibble(makeNibblePolygon(x, y));
+                if(dirty) {
+                    std::cout << "terrain changed!" << std::endl;
+                    terrain.updateGl();
+                }
+            }
+        } else
+            mousedown = false;
+
         // Graphics
         glClear( GL_COLOR_BUFFER_BIT );
+
+        terrain.draw(PV);
+
         for(int i=0;i<OBJS;++i) {
             glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(objects[i].position().x, objects[i].position().y, 0));
             ship1->render(PV * model);

8334810dbafcb46b696d98c056f6430f7e346db7
Calle Laakkonen <laakkonenc@gmail.com>
2012-11-28 20:50:50 +0200

    Copyright header.

--- src/terrain/common.h
+++ src/terrain/common.h
@@ -1,5 +1,6 @@
 //
 // This file is part of Luola2.
+// Copyright (C) 2012 Calle Laakkonen
 //
 // Luola2 is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
